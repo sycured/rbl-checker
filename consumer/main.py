878354f94c26f9195164e84e0475057f8f0ed447
sycured <60801403+sycured@users.noreply.github.com>
2021-01-10 21:00:12 -0500

    consumer ready

--- consumer/main.py
+++ consumer/main.py
@@ -1,27 +1,51 @@
-from rbls import rbls
+"""Consumes message from Kafka, check in RBL and insert positive IP."""
+from asyncio import get_event_loop, set_event_loop_policy
+from asyncio.events import AbstractEventLoop
 from datetime import datetime
-from requests import post
+
+from aiokafka import AIOKafkaConsumer
+
+from aiopg import Pool
+
+from db_utils import create_db_pool, insert_result
+
 from dns.resolver import resolve
 
+from kafka_utils import create_aio_consumer
 
-def do_job(crate_server, crate_port, rip):
-    """Run verification and if positive insert data to the database."""
-    ip = '.'.join(reversed(rip.split('.')))
-    for rblname in rbls.rbls:
-        try:
-            dns_query = rip + '.' + rblname
-            dns_result = resolve(dns_query, 'A')
+from rbls import rbls
+
+from uvloop import EventLoopPolicy
+
+set_event_loop_policy(EventLoopPolicy())
+loop: AbstractEventLoop = get_event_loop()
+
+
+def check(rip, rblname):
+    """Do and return the result of the query."""
+    try:
+        dns_query = f'{rip}.{rblname}'
+        return resolve(dns_query, 'A')
+    except Exception as e:
+        print(f'{e=}')
+        return None
+
+
+async def app():
+    """Do the check of the IP across the list and insert positive IP."""
+    aio_consumer: AIOKafkaConsumer = await create_aio_consumer(loop)
+    db_pool: Pool = await create_db_pool(loop)
+    await aio_consumer.start()
+    async for msg in aio_consumer:
+        if msg is None:
+            continue
+        ip = msg.value['ip']
+        rip = '.'.join(reversed(ip.split('.')))
+        for rblname in rbls:
             date = datetime.now().isoformat()
-            if dns_result[0]:
-                url = 'http://' + crate_server + ':' + crate_port + '/_sql'
-                k = ['date', 'ip_srv', 'rblname']
-                v = ["'{}', '{}', '{}'".format(date, ip, rblname)]
-                value = """INSERT INTO {} ({}) VALUES ({})""".format(
-                    'rbl',
-                    ', '.join(k),
-                    ', '.join(v)
-                )
-                payload = {'stmt': value}
-                post(url, json=payload)
-        except Exception as e:
-            print(f'error {e}')
+            result = check(rip, rblname)
+            if result is not None:
+                await insert_result(db_pool, date, ip, rblname)
+
+
+loop.run_until_complete(app())
