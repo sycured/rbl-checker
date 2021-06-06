3ad81897ac58ce23592ee976eda02adeb3f9b630
sycured <60801403+sycured@users.noreply.github.com>
2021-06-06 15:25:57 -0500

    add create_kafka_producer

--- rest_api_actix/src/main.rs
+++ rest_api_actix/src/main.rs
@@ -1,26 +1,27 @@
 use actix_web::middleware::{Compress, Logger};
-use actix_web::{App, HttpServer};
-use env_logger::Env;
+use actix_web::{web::Data, App, HttpServer};
+use env_logger::{Builder, Env};
 use paperclip::actix::OpenApiExt;
-use std::io::Result;
-
+use std::{io::Result, sync::Mutex};
 mod add;
 use add::init_routes;
-
 mod configuration;
 use configuration::{app_ip, app_port};
-
-// mod kafka;
+mod utils;
+use utils::create_kafka_producer;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    HttpServer::new(|| {
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+    let producer = create_kafka_producer().await;
+    let producer = Data::new(Mutex::new(producer));
+    HttpServer::new(move || {
         App::new()
             .wrap_api()
             .wrap(Compress::default())
             .wrap(Logger::default())
             .configure(init_routes)
+            .app_data(producer.clone())
             .with_json_spec_at("/openapi.json")
             .build()
     })
