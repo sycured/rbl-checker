846416271a741d468e7b4b19b27f5c557f7ba51f
sycured <60801403+sycured@users.noreply.github.com>
2021-06-06 15:25:21 -0500

    move from rdkafka to kafka + new function: create_kafka_producer

--- rest_api_actix/src/utils.rs
+++ rest_api_actix/src/utils.rs
@@ -1,6 +1,16 @@
-use log::error;
-use rdkafka::{
-    config::ClientConfig,
-    error::KafkaError,
-    producer::{FutureProducer, FutureRecord},
+use super::configuration::kafka_hosts;
+use kafka::producer::{
+    Compression::SNAPPY, DefaultHasher, DefaultPartitioner, Producer, RequiredAcks::One,
 };
+use std::{hash::BuildHasherDefault, time::Duration};
+
+pub async fn create_kafka_producer(
+) -> Producer<DefaultPartitioner<BuildHasherDefault<DefaultHasher>>> {
+    let producer = Producer::from_hosts(vec![kafka_hosts().await])
+        .with_compression(SNAPPY)
+        .with_ack_timeout(Duration::from_secs(1))
+        .with_required_acks(One)
+        .create()
+        .unwrap();
+    producer
+}
