df363cd08ff7c1585e483a1b5d8a887a78d55897
sycured <60801403+sycured@users.noreply.github.com>
2021-06-06 15:26:48 -0500

    fix code to call create_json + create_json send message to kafka

--- rest_api_actix/src/add.rs
+++ rest_api_actix/src/add.rs
@@ -1,9 +1,13 @@
 use actix_web::Result;
 use ipaddress::IPAddress;
-use log::info;
-use paperclip::actix::web::{post, resource, Json, ServiceConfig};
-use paperclip::actix::{api_v2_operation, Apiv2Schema};
+use kafka::producer::{Producer, Record};
+use paperclip::actix::{
+    api_v2_operation,
+    web::{post, resource, Data, Json, ServiceConfig},
+    Apiv2Schema,
+};
 use serde::{Deserialize, Serialize};
+use std::{ops::DerefMut, sync::Mutex};
 
 #[derive(Deserialize, Apiv2Schema)]
 pub struct AddPost {
@@ -15,8 +19,13 @@ pub struct AddResp {
     result: String,
 }
 
-fn create_json(data: String) {
-    info!("{}", Json(AddResp { result: data }).result)
+fn create_json(data: String, producer: &mut Producer) {
+    producer
+        .send(&Record::from_value(
+            "rbl",
+            format!("{}", Json(AddResp { result: data }).result),
+        ))
+        .unwrap();
 }
 
 #[api_v2_operation(
@@ -24,12 +33,12 @@ fn create_json(data: String) {
     consumes = "application/json",
     produces = "application/json"
 )]
-pub async fn add(data: Json<AddPost>) -> Result<Json<AddResp>> {
+pub async fn add(data: Json<AddPost>, producer: Data<Mutex<Producer>>) -> Result<Json<AddResp>> {
     let ipr = IPAddress::parse(data.ip_range.to_string()).unwrap();
     if !ipr.to_string().contains("/32") {
-        ipr.each_host(|ip| create_json(ip.to_string()))
+        ipr.each_host(|ip| create_json(ip.to_string(), producer.lock().unwrap().deref_mut()))
     } else {
-        create_json(ipr.to_string())
+        create_json(ipr.to_string(), producer.lock().unwrap().deref_mut());
     }
     Ok(Json(AddResp {
         result: format!("Success, new IP range added: {}", ipr.to_string()),
