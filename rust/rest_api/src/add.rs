a93fb5ad2c6c6fde115aaadd7aeb577107b870d6
sycured <60801403+sycured@users.noreply.github.com>
2021-06-08 15:16:16 -0500

    fix json sent to kafka

--- rust/rest_api/src/add.rs
+++ rust/rest_api/src/add.rs
@@ -15,15 +15,20 @@ pub struct AddPost {
 }
 
 #[derive(Serialize, Apiv2Schema)]
-pub struct AddResp {
-    result: String,
+pub struct AddIpToKafka {
+    ip: String,
+}
+
+#[derive(Serialize, Apiv2Schema)]
+pub struct Response {
+    status: String,
 }
 
 fn create_json(data: String, producer: &mut Producer) {
     producer
         .send(&Record::from_value(
             "rbl",
-            format!("{}", Json(AddResp { result: data }).result),
+            serde_json::to_string(&AddIpToKafka { ip: data }).unwrap(),
         ))
         .unwrap();
 }
@@ -33,18 +38,16 @@ fn create_json(data: String, producer: &mut Producer) {
     consumes = "application/json",
     produces = "application/json"
 )]
-pub async fn add(data: Json<AddPost>, producer: Data<Mutex<Producer>>) -> Result<Json<AddResp>> {
+pub async fn add(data: Json<AddPost>, producer: Data<Mutex<Producer>>) -> Result<Json<Response>> {
     let ipr = IPAddress::parse(data.ip_range.to_string()).unwrap();
 
     match ipr.to_string().contains("/32") {
-        true => create_json(ipr.to_string(), producer.lock().unwrap().deref_mut()),
-        false => {
-            ipr.each_host(|ip| create_json(ip.to_string(), producer.lock().unwrap().deref_mut()))
-        }
+        true => create_json(ipr.to_s(), producer.lock().unwrap().deref_mut()),
+        false => ipr.each_host(|ip| create_json(ip.to_s(), producer.lock().unwrap().deref_mut())),
     }
 
-    Ok(Json(AddResp {
-        result: format!("Success, new IP range added: {}", ipr.to_string()),
+    Ok(Json(Response {
+        status: format!("Success, new IP range added: {}", ipr.to_string()),
     }))
 }
 
